e44e48e55891877fdda332ed36a6150c9b714fb6
Crowdbotics <team@crowdbotics.com>
2022-11-29 21:52:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "data_tracker_templa_37746",
+  url: "https://data_tracker_templa_37746.botics.co",
+  api: "https://data_tracker_templa_37746.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
